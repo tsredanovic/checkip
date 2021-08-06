ad216f280ae2b93356e10426f023e46df4e606b6
Toni Sredanović <toni@krakensystems.co>
2021-08-06 18:55:55 +0200

    resolve_ip

--- src/checkip/ip.py
+++ src/checkip/ip.py
@@ -1,3 +1,6 @@
+import threading
+from typing import Counter
+
 from checkip.exceptions import InvalidProviderError
 from checkip.providers import providers_dict
 
@@ -10,3 +13,24 @@ def get_provider_instance(provider_code):
 def get_ip(provider):
     provider = get_provider_instance(provider)
     return provider.get_ip()
+
+
+def get_ip_thread(provider, results):
+    ip = get_ip(provider)
+    results[provider] = ip
+
+
+def resolve_ip(providers):  # TODO add strict mode where number of most commong IP is same sa len(providers)
+    results = {}
+    all_threads = []
+    for provider in providers:
+        t = threading.Thread(target=get_ip_thread, args=(provider, results))
+        t.start()
+        all_threads.append(t)
+
+    for t in all_threads:
+        t.join()
+    
+    ip_counts = Counter(results.values())
+
+    return ip_counts.most_common()[0][0]  # TODO add exception for when ip_counts.most_common() == []
